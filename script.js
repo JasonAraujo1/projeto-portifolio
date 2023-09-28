6fae44ee8bbd42b44f51e24ccc0c9d9f5933f1c9
Jason A. <you@example.com>
2023-09-28 02:27:34 -0300

    box projects adjust

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+
+const carouselContainer = document.getElementById('carousel-container');
 const iframeCarousel = document.getElementById('carousel');
 const iframes = iframeCarousel.querySelectorAll('iframe');
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
 
 let currentIndex = 0;
+const intervalDuration = 5000; // 5 segundos
 
 function showIframe(index) {
   iframes.forEach((iframe, i) => {
@@ -11,14 +14,34 @@ function showIframe(index) {
   });
 }
 
-prevButton.addEventListener('click', () => {
-  currentIndex = (currentIndex - 1 + iframes.length) % iframes.length;
-  showIframe(currentIndex);
-});
-
-nextButton.addEventListener('click', () => {
+function nextSlide() {
   currentIndex = (currentIndex + 1) % iframes.length;
   showIframe(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + iframes.length) % iframes.length;
+  showIframe(currentIndex);
+}
+
+nextButton.addEventListener('click', () => {
+  nextSlide();
+  clearInterval(carouselInterval);
+});
+
+prevButton.addEventListener('click', () => {
+  prevSlide();
+  clearInterval(carouselInterval);
 });
 
 showIframe(currentIndex);
+
+let carouselInterval = setInterval(nextSlide, intervalDuration);
+
+carouselContainer.addEventListener('mouseenter', () => {
+  clearInterval(carouselInterval);
+});
+
+carouselContainer.addEventListener('mouseleave', () => {
+  carouselInterval = setInterval(nextSlide, intervalDuration);
+});
