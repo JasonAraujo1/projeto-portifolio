98643e851813a000556b625b2d4d6806384dff5f
Jason A. <you@example.com>
2023-10-02 23:28:07 -0300

    imgs html size adjust phone desktop

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const carouselContainer = document.querySelector('#carousel');
-const images = carouselContainer.querySelectorAll('img');
+const images = carouselContainer.querySelectorAll('img.slide');
 const prevButton = document.querySelector('#prev');
 const nextButton = document.querySelector('#next');
 const playPauseButton = document.querySelector('#playPause');
+
+const phoneImages = [
+  'assets/dev-links-thumbnail-phone.jpg',
+  'assets/social-tree-thumbnail-phone.png',
+  'assets/brownie-thumbnail-phone.png',
+];
+
+const desktopImages = [
+  'assets/dev-links-thumbnail.jpg',
+  'assets/social-tree-thumbnail.png',
+  'assets/brownie-thumbnail.png',
+];
 
 let currentIndex = 0;
 const intervalDuration = 10000;
@@ -18,6 +30,12 @@ function nextSlide() {
   if (!isPaused) {
     currentIndex = (currentIndex + 1) % images.length;
     showImage(currentIndex);
+
+    // Verifique se todas as imagens do conjunto atual foram exibidas
+    if (currentIndex === 0) {
+      // Reinicie o loop
+      setTimeout(nextSlide, intervalDuration);
+    }
   }
 }
 
@@ -28,7 +46,25 @@ function prevSlide() {
   }
 }
 
+function updateImagesBasedOnScreenWidth() {
+  const screenWidth = window.innerWidth;
 
+  if (screenWidth < 660) {
+    images.forEach((img, index) => {
+      img.src = phoneImages[index];
+    });
+  } else {
+    images.forEach((img, index) => {
+      img.src = desktopImages[index];
+    });
+  }
+
+  showImage(currentIndex);
+}
+
+updateImagesBasedOnScreenWidth();
+
+window.addEventListener('resize', updateImagesBasedOnScreenWidth);
 
 showImage(currentIndex);
 
@@ -43,5 +79,3 @@ carouselContainer.addEventListener('mouseleave', () => {
     carouselInterval = setInterval(nextSlide, intervalDuration);
   }
 });
-
-
